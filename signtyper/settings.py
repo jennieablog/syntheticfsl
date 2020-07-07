2b32464e0c434e8886c49721ea24313a757a30ec
Jennie Ron Ablog <jennieablog@gmail.com>
2020-07-07 16:25:15 +0800

    Removed static files firs

--- signtyper/settings.py
+++ signtyper/settings.py
@@ -126,10 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Add this line.
 LOGIN_REDIRECT_URL = '/inventory/handshapes'
 LOGOUT_REDIRECT_URL = '/inventory/handshapes'
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- signtyper/urls.py
+++ signtyper/urls.py
@@ -34,6 +34,6 @@ urlpatterns += [
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# from django.conf import settings
+# from django.conf.urls.static import static
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
