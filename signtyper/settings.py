514fabd68e03dbe7be0d1b1390bfb6baf80aba18
Jennie Ron Ablog <jennieablog@gmail.com>
2020-07-11 10:41:31 +0800

    Include pychache folders in gitignore; Change key and keep secret

--- signtyper/settings.py
+++ signtyper/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@58!z%axj)^=pt^e74=uw%i4r1*n%d%-+6uer^ho95(_@%q-_k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'signtyper.herokuapp.com']
 
